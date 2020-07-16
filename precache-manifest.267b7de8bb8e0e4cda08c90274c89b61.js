9e73f1b268c61cf98329e5732dfa892785a3d730
Maciej Panocha <maciej.panocha@gmail.com>
2020-07-16 10:24:27 +0200

    Updates

---
+++ precache-manifest.267b7de8bb8e0e4cda08c90274c89b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ace9a6be71334d9295f9398466b90d1",
+    "url": "/todos-react/index.html"
+  },
+  {
+    "revision": "aacfacb945792fdb105d",
+    "url": "/todos-react/static/css/main.ce717c5f.chunk.css"
+  },
+  {
+    "revision": "ddca7595ca5b88461923",
+    "url": "/todos-react/static/js/2.c241509a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos-react/static/js/2.c241509a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aacfacb945792fdb105d",
+    "url": "/todos-react/static/js/main.be1d9b12.chunk.js"
+  },
+  {
+    "revision": "2b607e1ae607ce9060ea",
+    "url": "/todos-react/static/js/runtime-main.30f2425a.js"
+  }
+]);
